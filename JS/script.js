0d90c24a78360e9e739f0f1763026543195f0019
syed-faakhir-shah <syedfaakhirshah@gmail.com>
2024-12-21 15:25:48 +0500

    Add files via upload

---
+++ JS/script.js
@@ -0,0 +1,7 @@
+// Example: Simple JavaScript for form validation or adding functionality
+document.addEventListener("DOMContentLoaded", function() {
+    // Code to add more dynamic features (e.g., validation, modals, etc.)
+});
+
+
+
